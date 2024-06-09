161e7f7050abcaee6519d355fe5abeff167f69df
khaidinhdang <119165279+khaidinhdang@users.noreply.github.com>
2024-06-09 22:46:39 +0700

    Update ForgotPasswordScreen.kt

--- app/src/main/java/com/example/courseapp/screens/Auth/ForgotPasswordScreen.kt
+++ app/src/main/java/com/example/courseapp/screens/Auth/ForgotPasswordScreen.kt
@@ -1,1 +1,166 @@
-package com.example.courseapp.screens.Authimport android.widget.Toastimport androidx.compose.foundation.backgroundimport androidx.compose.foundation.layout.Arrangementimport androidx.compose.foundation.layout.Columnimport androidx.compose.foundation.layout.Spacerimport androidx.compose.foundation.layout.fillMaxSizeimport androidx.compose.foundation.layout.fillMaxWidthimport androidx.compose.foundation.layout.heightimport androidx.compose.foundation.layout.paddingimport androidx.compose.foundation.layout.sizeimport androidx.compose.foundation.rememberScrollStateimport androidx.compose.foundation.shape.RoundedCornerShapeimport androidx.compose.foundation.verticalScrollimport androidx.compose.material3.Buttonimport androidx.compose.material.Textimport androidx.compose.material.TextFieldimport androidx.compose.material.TextFieldDefaultsimport androidx.compose.material.icons.Iconsimport androidx.compose.material.icons.outlined.Lockimport androidx.compose.material3.BottomSheetDefaultsimport androidx.compose.material3.ButtonDefaultsimport androidx.compose.material3.ExperimentalMaterial3Apiimport androidx.compose.material3.Iconimport androidx.compose.material3.ModalBottomSheetimport androidx.compose.material3.rememberModalBottomSheetStateimport androidx.compose.runtime.Composableimport androidx.compose.runtime.getValueimport androidx.compose.runtime.livedata.observeAsStateimport androidx.compose.runtime.mutableStateOfimport androidx.compose.runtime.saveable.rememberSaveableimport androidx.compose.runtime.setValueimport androidx.compose.ui.Alignmentimport androidx.compose.ui.Modifierimport androidx.compose.ui.graphics.Colorimport androidx.compose.ui.platform.LocalContextimport androidx.compose.ui.text.font.FontWeightimport androidx.compose.ui.text.style.TextAlignimport androidx.compose.ui.unit.dpimport androidx.compose.ui.unit.spimport androidx.lifecycle.viewmodel.compose.viewModelimport androidx.navigation.NavHostControllerimport com.example.courseapp.ui.theme.white80import com.example.courseapp.ViewModel.AuthViewModel@OptIn(ExperimentalMaterial3Api::class)@Composablefun ForgotPasswordScreen(    navController: NavHostController,    onDismissed: () -> Unit,) {    var email by rememberSaveable {        mutableStateOf("")    }    val authViewModel: AuthViewModel = viewModel()    val firebaseUser by authViewModel.firebaseUser.observeAsState()    val error by authViewModel.error.observeAsState()    val context = LocalContext.current    val modalBottomSheetState = rememberModalBottomSheetState()    ModalBottomSheet(        onDismissRequest = { onDismissed() },        sheetState = modalBottomSheetState,        dragHandle = { BottomSheetDefaults.DragHandle() },    ) {        Column(            horizontalAlignment = Alignment.CenterHorizontally,            modifier = Modifier                .fillMaxSize()                .verticalScroll(rememberScrollState())                .background(white80)                .padding(top = 25.dp, start = 25.dp, end = 25.dp)        ) {            Column(                horizontalAlignment = Alignment.Start,                modifier = Modifier.fillMaxWidth()            ) {                Text(                    text = "Forgot Password",                    fontSize = 30.sp,                    fontWeight = FontWeight.Bold                )                Spacer(modifier = Modifier.height(10.dp))                Text(                    text = "Please enter your email to reset your password",                    fontWeight = FontWeight.SemiBold,                    fontSize = 14.sp,                    color = Color(0xFFB2B9CA)                )            }            Spacer(modifier = Modifier.height(35.dp))            Column(                horizontalAlignment = Alignment.Start,                verticalArrangement = Arrangement.Top            ) {                Text(                    text = "Email",                    fontWeight = FontWeight.Bold,                    fontSize = 15.sp,                    textAlign = TextAlign.Left                )                Spacer(modifier = Modifier.height(10.dp))                TextField(                    value = email,                    onValueChange = { email = it },                    label = {                        Text(                            text = "Enter your email",                            fontWeight = FontWeight.SemiBold,                            fontSize = 14.sp,                            color = Color(0xFFB2B9CA)                        )                    },                    leadingIcon = {                        Icon(imageVector = Icons.Outlined.Lock, contentDescription = null)                    },                    colors = TextFieldDefaults.textFieldColors(                        unfocusedIndicatorColor = Color.Transparent,                        backgroundColor = Color(0xFFFBFBFD),                        cursorColor = Color(0xFFB2B9CA),                    ),                    modifier = Modifier.size(366.dp, 56.dp),                )                Spacer(modifier = Modifier.height(20.dp))                Button(                    onClick = {                        if (email.isEmpty()) {                            Toast.makeText(context, "Please enter your email", Toast.LENGTH_SHORT).show()                        } else {                            authViewModel.forgot(email = email)                            onDismissed()                        }                    },                    modifier = Modifier                        .size(366.dp, 56.dp)                        .background(color = Color(0xFFFF8450), shape = RoundedCornerShape(20.dp)),                    colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),                    shape = RoundedCornerShape(20.dp)                ) {                    Text(                        text = "Reset Password",                        fontWeight = FontWeight.SemiBold,                        fontSize = 16.sp,                        color = Color.White                    )                }            }        }        // Observe ViewModel state and show Toast messages accordingly        firebaseUser?.let {            if (it != null) {                Toast.makeText(context, "Password reset email sent", Toast.LENGTH_SHORT).show()                onDismissed()            }        }        error?.let {            if (it.isNotEmpty()) {                Toast.makeText(context, it, Toast.LENGTH_SHORT).show()            }        }    }}
+package com.example.courseapp.screens.Auth
+
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Lock
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import com.example.courseapp.ui.theme.white80
+import com.example.courseapp.ViewModel.AuthViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ForgotPasswordScreen(
+    navController: NavHostController,
+    onDismissed: () -> Unit,
+) {
+    var email by rememberSaveable {
+        mutableStateOf("")
+    }
+    val authViewModel: AuthViewModel = viewModel()
+    val firebaseUser by authViewModel.firebaseUser.observeAsState()
+    val error by authViewModel.error.observeAsState()
+    val context = LocalContext.current
+    val modalBottomSheetState = rememberModalBottomSheetState()
+
+    ModalBottomSheet(
+        onDismissRequest = { onDismissed() },
+        sheetState = modalBottomSheetState,
+        dragHandle = { BottomSheetDefaults.DragHandle() },
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .background(white80)
+                .padding(top = 25.dp, start = 25.dp, end = 25.dp)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.Start,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "Forgot Password",
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                Text(
+                    text = "Please enter your email to reset your password",
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 14.sp,
+                    color = Color(0xFFB2B9CA)
+                )
+            }
+
+            Spacer(modifier = Modifier.height(35.dp))
+            Column(
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.Top
+            ) {
+                Text(
+                    text = "Email",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 15.sp,
+                    textAlign = TextAlign.Left
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                TextField(
+                    value = email,
+                    onValueChange = { email = it },
+                    label = {
+                        Text(
+                            text = "Enter your email",
+                            fontWeight = FontWeight.SemiBold,
+                            fontSize = 14.sp,
+                            color = Color(0xFFB2B9CA)
+                        )
+                    },
+                    leadingIcon = {
+                        Icon(imageVector = Icons.Outlined.Lock, contentDescription = null)
+                    },
+                    colors = TextFieldDefaults.textFieldColors(
+                        unfocusedIndicatorColor = Color.Transparent,
+                        backgroundColor = Color(0xFFFBFBFD),
+                        cursorColor = Color(0xFFB2B9CA),
+                    ),
+                    modifier = Modifier.size(366.dp, 56.dp),
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                Button(
+                    onClick = {
+                        if (email.isEmpty()) {
+                            Toast.makeText(context, "Please enter your email", Toast.LENGTH_SHORT).show()
+                        } else {
+                            authViewModel.forgot(email = email)
+                            onDismissed()
+                        }
+                    },
+                    modifier = Modifier
+                        .size(366.dp, 56.dp)
+                        .background(color = Color(0xFFFF8450), shape = RoundedCornerShape(20.dp)),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+                    shape = RoundedCornerShape(20.dp)
+                ) {
+                    Text(
+                        text = "Reset Password",
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 16.sp,
+                        color = Color.White
+                    )
+                }
+            }
+        }
+
+        // Observe ViewModel state and show Toast messages accordingly
+        firebaseUser?.let {
+            if (it != null) {
+                Toast.makeText(context, "Password reset email sent", Toast.LENGTH_SHORT).show()
+                onDismissed()
+            }
+        }
+
+        error?.let {
+            if (it.isNotEmpty()) {
+                Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
