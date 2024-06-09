2617c84ec71082b80e9ab176baeca41d0439c1ce
khaidinhdang <119165279+khaidinhdang@users.noreply.github.com>
2024-06-09 22:46:50 +0700

    Update LoginScreen.kt

--- app/src/main/java/com/example/courseapp/screens/Auth/LoginScreen.kt
+++ app/src/main/java/com/example/courseapp/screens/Auth/LoginScreen.kt
@@ -1,1 +1,279 @@
-package com.example.courseapp.screens.Authimport android.widget.Toastimport androidx.compose.foundation.backgroundimport androidx.compose.foundation.layout.*import androidx.compose.foundation.rememberScrollStateimport androidx.compose.foundation.shape.RoundedCornerShapeimport androidx.compose.foundation.verticalScrollimport androidx.compose.material.TextFieldimport androidx.compose.material.TextFieldDefaultsimport androidx.compose.material.icons.Iconsimport androidx.compose.material.icons.outlined.Emailimport androidx.compose.material.icons.outlined.Lockimport androidx.compose.material3.*import androidx.compose.runtime.*import androidx.compose.runtime.livedata.observeAsStateimport androidx.compose.ui.Alignmentimport androidx.compose.ui.Modifierimport androidx.compose.ui.graphics.Colorimport androidx.compose.ui.platform.LocalContextimport androidx.compose.ui.text.font.FontWeightimport androidx.compose.ui.text.input.PasswordVisualTransformationimport androidx.compose.ui.text.style.TextAlignimport androidx.compose.ui.tooling.preview.Previewimport androidx.compose.ui.unit.dpimport androidx.compose.ui.unit.spimport androidx.lifecycle.viewmodel.compose.viewModelimport androidx.navigation.NavHostControllerimport androidx.navigation.compose.rememberNavControllerimport com.example.courseapp.navigation.Routesimport com.example.courseapp.ui.theme.black80import com.example.courseapp.ui.theme.white80import com.example.courseapp.ViewModel.AuthViewModel@Composablefun LoginScreen(navController: NavHostController) {    val authViewModel: AuthViewModel = viewModel()    val firebaseUser by authViewModel.firebaseUser.observeAsState()    val error by authViewModel.error.observeAsState()    val context = LocalContext.current    LaunchedEffect(firebaseUser) {        if (firebaseUser != null) {            navController.navigate(Routes.BottomNav.routes) {                popUpTo(navController.graph.startDestinationId)                launchSingleTop = true            }        }    }    LaunchedEffect(error) {        error?.let {            Toast.makeText(context, it, Toast.LENGTH_SHORT).show()        }    }    var email by remember { mutableStateOf("") }    var password by remember { mutableStateOf("") }    var checked by remember { mutableStateOf(false) }    var showSheet by remember { mutableStateOf(false) }    if (showSheet) {        ForgotPasswordScreen(            onDismissed = {                showSheet = false            },            navController = navController        )    }    Column(        horizontalAlignment = Alignment.CenterHorizontally,        modifier = Modifier            .fillMaxSize()            .verticalScroll(rememberScrollState())            .background(white80)            .padding(top = 25.dp, start = 25.dp, end = 25.dp)    ) {        Column(            horizontalAlignment = Alignment.Start,            modifier = Modifier.fillMaxWidth()        ) {            Text(                text = "Login Account",                fontSize = 30.sp,                fontWeight = FontWeight.Bold            )            Spacer(modifier = Modifier.height(10.dp))            Text(                text = "Please login with your registered account",                fontWeight = FontWeight.SemiBold,                fontSize = 14.sp,                color = Color(0xFFB2B9CA)            )        }        Spacer(modifier = Modifier.height(35.dp))        Column(            horizontalAlignment = Alignment.Start,            verticalArrangement = Arrangement.Top        ) {            Text(                text = "Email",                fontWeight = FontWeight.Bold,                fontSize = 15.sp,                textAlign = TextAlign.Left            )            Spacer(modifier = Modifier.height(10.dp))            TextField(                value = email,                onValueChange = {                    email = it                },                label = {                    Text(                        text = "Enter your email",                        fontWeight = FontWeight.SemiBold,                        fontSize = 14.sp,                        color = Color(0xFFB2B9CA)                    )                },                leadingIcon = {                    Icon(imageVector = Icons.Outlined.Email, contentDescription = null)                },                colors = TextFieldDefaults.textFieldColors(                    unfocusedIndicatorColor = Color.Transparent,                    backgroundColor = Color(0xFFFBFBFD),                    cursorColor = Color(0xFFB2B9CA),                ),                modifier = Modifier.size(366.dp, 56.dp),            )        }        Spacer(modifier = Modifier.height(10.dp))        Column(            horizontalAlignment = Alignment.Start,            verticalArrangement = Arrangement.Top        ) {            Text(                text = "Password",                fontWeight = FontWeight.Bold,                fontSize = 15.sp,                textAlign = TextAlign.Left            )            Spacer(modifier = Modifier.height(10.dp))            TextField(                value = password,                onValueChange = { password = it },                label = {                    Text(                        text = "Enter your Password",                        fontWeight = FontWeight.SemiBold,                        fontSize = 14.sp,                        color = Color(0xFFB2B9CA)                    )                },                leadingIcon = {                    Icon(imageVector = Icons.Outlined.Lock, contentDescription = null)                },                colors = TextFieldDefaults.textFieldColors(                    unfocusedIndicatorColor = Color.Transparent,                    backgroundColor = Color(0xFFFBFBFD),                    cursorColor = Color(0xFFB2B9CA),                ),                modifier = Modifier.size(366.dp, 56.dp),                visualTransformation = PasswordVisualTransformation(),            )        }        Spacer(modifier = Modifier.height(10.dp))        Row(            modifier = Modifier.fillMaxWidth(),            horizontalArrangement = Arrangement.SpaceBetween        ) {            Row(                horizontalArrangement = Arrangement.Center,                verticalAlignment = Alignment.CenterVertically            ) {                Checkbox(                    checked = checked,                    onCheckedChange = { checked = it }                )                Text(                    text = "Remember me",                    fontWeight = FontWeight.Bold,                    fontSize = 15.sp                )            }            TextButton(                onClick = {                    showSheet = true                }            ) {                Text(                    text = "Forgot Password?",                    fontWeight = FontWeight.SemiBold,                    fontSize = 14.sp,                    color = Color(0xFFFF8450),                )            }        }        Spacer(modifier = Modifier.height(20.dp))        Button(            onClick = {                if (email.isNotEmpty() && password.isNotEmpty()) {                    authViewModel.login(email, password, context)                } else {                    Toast.makeText(context, "Please provide all fields", Toast.LENGTH_SHORT).show()                }            },            modifier = Modifier                .size(366.dp, 56.dp)                .background(color = Color(0xFFFF8450), shape = RoundedCornerShape(20.dp)),            colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),            shape = RoundedCornerShape(20.dp)        ) {            Text(                text = "Login",                fontWeight = FontWeight.SemiBold,                fontSize = 16.sp,                color = Color.White            )        }        Spacer(modifier = Modifier.height(10.dp))        Column(            modifier = Modifier.fillMaxSize(),            horizontalAlignment = Alignment.CenterHorizontally,            verticalArrangement = Arrangement.Bottom        ) {            Column(                modifier = Modifier.fillMaxWidth(),                verticalArrangement = Arrangement.Center,                horizontalAlignment = Alignment.CenterHorizontally            ) {                Row(                    horizontalArrangement = Arrangement.Center,                    verticalAlignment = Alignment.CenterVertically                ) {                    Text(                        text = "Don't have an Account?",                        fontWeight = FontWeight.SemiBold,                        fontSize = 14.sp,                        color = black80                    )                    TextButton(                        onClick = {                            navController.navigate(Routes.Register.routes) {                                popUpTo(navController.graph.startDestinationId)                                launchSingleTop = true                            }                        }                    ) {                        Text(                            text = "Sign Up",                            fontWeight = FontWeight.SemiBold,                            fontSize = 14.sp,                            color = Color(0xFFFF8450),                        )                    }                }            }        }    }}@Preview(showSystemUi = true)@Composablefun LoginView() {    // Provide a dummy NavHostController for the preview    val navController = rememberNavController()    LoginScreen(navController)}
+package com.example.courseapp.screens.Auth
+
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Lock
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.courseapp.navigation.Routes
+import com.example.courseapp.ui.theme.black80
+import com.example.courseapp.ui.theme.white80
+import com.example.courseapp.ViewModel.AuthViewModel
+
+@Composable
+fun LoginScreen(navController: NavHostController) {
+    val authViewModel: AuthViewModel = viewModel()
+    val firebaseUser by authViewModel.firebaseUser.observeAsState()
+    val error by authViewModel.error.observeAsState()
+    val context = LocalContext.current
+
+    LaunchedEffect(firebaseUser) {
+        if (firebaseUser != null) {
+            navController.navigate(Routes.BottomNav.routes) {
+                popUpTo(navController.graph.startDestinationId)
+                launchSingleTop = true
+            }
+        }
+    }
+
+    LaunchedEffect(error) {
+        error?.let {
+            Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var checked by remember { mutableStateOf(false) }
+    var showSheet by remember { mutableStateOf(false) }
+
+    if (showSheet) {
+        ForgotPasswordScreen(
+            onDismissed = {
+                showSheet = false
+            },
+            navController = navController
+        )
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .background(white80)
+            .padding(top = 25.dp, start = 25.dp, end = 25.dp)
+    ) {
+        Column(
+            horizontalAlignment = Alignment.Start,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = "Login Account",
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(
+                text = "Please login with your registered account",
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 14.sp,
+                color = Color(0xFFB2B9CA)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(35.dp))
+
+        Column(
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Top
+        ) {
+            Text(
+                text = "Email",
+                fontWeight = FontWeight.Bold,
+                fontSize = 15.sp,
+                textAlign = TextAlign.Left
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            TextField(
+                value = email,
+                onValueChange = {
+                    email = it
+                },
+                label = {
+                    Text(
+                        text = "Enter your email",
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 14.sp,
+                        color = Color(0xFFB2B9CA)
+                    )
+                },
+                leadingIcon = {
+                    Icon(imageVector = Icons.Outlined.Email, contentDescription = null)
+                },
+                colors = TextFieldDefaults.textFieldColors(
+                    unfocusedIndicatorColor = Color.Transparent,
+                    backgroundColor = Color(0xFFFBFBFD),
+                    cursorColor = Color(0xFFB2B9CA),
+                ),
+                modifier = Modifier.size(366.dp, 56.dp),
+            )
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Column(
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Top
+        ) {
+            Text(
+                text = "Password",
+                fontWeight = FontWeight.Bold,
+                fontSize = 15.sp,
+                textAlign = TextAlign.Left
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                label = {
+                    Text(
+                        text = "Enter your Password",
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 14.sp,
+                        color = Color(0xFFB2B9CA)
+                    )
+                },
+                leadingIcon = {
+                    Icon(imageVector = Icons.Outlined.Lock, contentDescription = null)
+                },
+                colors = TextFieldDefaults.textFieldColors(
+                    unfocusedIndicatorColor = Color.Transparent,
+                    backgroundColor = Color(0xFFFBFBFD),
+                    cursorColor = Color(0xFFB2B9CA),
+                ),
+                modifier = Modifier.size(366.dp, 56.dp),
+                visualTransformation = PasswordVisualTransformation(),
+            )
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(
+                    checked = checked,
+                    onCheckedChange = { checked = it }
+                )
+                Text(
+                    text = "Remember me",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 15.sp
+                )
+            }
+            TextButton(
+                onClick = {
+                    showSheet = true
+                }
+            ) {
+                Text(
+                    text = "Forgot Password?",
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 14.sp,
+                    color = Color(0xFFFF8450),
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            onClick = {
+                if (email.isNotEmpty() && password.isNotEmpty()) {
+                    authViewModel.login(email, password, context)
+                } else {
+                    Toast.makeText(context, "Please provide all fields", Toast.LENGTH_SHORT).show()
+                }
+            },
+            modifier = Modifier
+                .size(366.dp, 56.dp)
+                .background(color = Color(0xFFFF8450), shape = RoundedCornerShape(20.dp)),
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+            shape = RoundedCornerShape(20.dp)
+        ) {
+            Text(
+                text = "Login",
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 16.sp,
+                color = Color.White
+            )
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "Don't have an Account?",
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 14.sp,
+                        color = black80
+                    )
+                    TextButton(
+                        onClick = {
+                            navController.navigate(Routes.Register.routes) {
+                                popUpTo(navController.graph.startDestinationId)
+                                launchSingleTop = true
+                            }
+                        }
+                    ) {
+                        Text(
+                            text = "Sign Up",
+                            fontWeight = FontWeight.SemiBold,
+                            fontSize = 14.sp,
+                            color = Color(0xFFFF8450),
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun LoginView() {
+    // Provide a dummy NavHostController for the preview
+    val navController = rememberNavController()
+    LoginScreen(navController)
+}
