eb68da78c087f4d7dba8dc8d24a6d5fc7f3478ae
khaidinhdang <119165279+khaidinhdang@users.noreply.github.com>
2024-06-16 19:03:56 +0700

    Update Notes.kt

--- app/src/main/java/com/example/courseapp/model/Notes.kt
+++ app/src/main/java/com/example/courseapp/model/Notes.kt
@@ -1,1 +1,8 @@
-package com.example.courseapp.modeldata class Notes(    val id: String ="",    val title:String ="",    val description:String="",    var timestamp: Long = 0,)
+package com.example.courseapp.model
+
+data class Notes(
+    val id: String ="",
+    val title:String ="",
+    val description:String="",
+    var timestamp: Long = 0,
+)
